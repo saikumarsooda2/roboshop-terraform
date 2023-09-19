305d1dfbb54be6adb661c506af3f749cfb0a2d31
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-19 14:49:45 +0100

    added the prometheus ec2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "instance" {
   for_each  = var.component
   source    = "git::https://github.com/saikumarsooda2/tf-module-app.git"
-  component = each.key
+  component = each.key["component"]
   env       = var.env
   tags       = each.value["tags"]
 }
