0f67d78899a6fe3213c8d868eb6cac9c4d878fc7
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-25 18:19:09 +0100

    added the prometheus ec2

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "aws" {
+module "vpc" {
   for_each = var.vpc
   source = "git::https://github.com/saikumarsooda2/tf-vpc.git"
-  cidr_block = each.value["cidr_block"]
+  vpc = each.value["cidr_block"]
   tags = var.tags
 }
