c96248bf4eadcbb98258e4d09031cb5066d2e7ce
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-28 17:14:40 +0100

    added the prometheus ec2

--- main.tf
+++ main.tf
@@ -11,3 +11,10 @@ module "vpc" {
 output "subnet_ids" {
   value = module.vpc
 }
+
+module "app-server" {
+  type = "t3.micro"
+  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main",null ), "subnet_ids", null), "app",null), "subnet_ids",null)[0]
+  vpc_id = lookup(lookup(module.vpc, main, null ), "vpc_id", null)
+
+}
