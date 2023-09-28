f5fca1595eba3113325458736d4f77a1c65c936d
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-09-28 17:18:21 +0100

    added the prometheus ec2

--- main.tf
+++ main.tf
@@ -14,9 +14,10 @@ output "subnet_ids" {
 
 module "app-server" {
   source = "git::https://github.com/saikumarsooda2/tf-module-app.git"
+  component = "test"
+  env = "dev"
   type = "t3.micro"
   subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main",null ), "subnet_ids", null), "app",null), "subnet_ids",null)[0]
-  vpc_id = lookup(lookup(module.vpc, main, null ), "vpc_id", null)
 
 }
 
