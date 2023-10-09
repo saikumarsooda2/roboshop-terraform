7174938f17271b125d2247f4b62b39ca81a87573
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-10-09 11:24:47 +0100

    added the prometheus ec2

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ module "vpc" {
   tags       = var.tags
   default_vpc = var.default_vpc
 }
-
-output "subnet_ids" {
-  value = module.vpc
-}
-
-module "app-server" {
-  source = "git::https://github.com/saikumarsooda2/tf-module-app.git"
-  component = "test"
-  env = "dev"
-  type = "t3.micro"
-  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main",null ), "subnet_ids", null), "app",null), "subnet_ids",null)[0]
-  vpc_id = lookup(lookup(module.vpc, "main",null), "vpc_id",null)[0]
-
-}
+#
+#output "subnet_ids" {
+#  value = module.vpc
+#}
+#
+#module "app-server" {
+#  source = "git::https://github.com/saikumarsooda2/tf-module-app.git"
+#  component = "test"
+#  env = "dev"
+#  type = "t3.micro"
+#  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main",null ), "subnet_ids", null), "app",null), "subnet_ids",null)[0]
+#  vpc_id = lookup(lookup(module.vpc, "main",null), "vpc_id",null)[0]
+#
+#}
 
 
