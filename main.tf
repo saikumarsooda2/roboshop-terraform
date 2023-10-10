56aab69ffe81b3dc06e868fe3c609207658b08d1
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-10-10 12:36:14 +0100

    removed everything for restart

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@
 module "instances" {
   for_each = var.component
   source = "git::https://github.com/saikumarsooda2/tf-module-app.git"
-  component = each.value["component"]
-  env = "${var.env}"
+  component = each.key["component"]
+  env = var.env
 }
 
